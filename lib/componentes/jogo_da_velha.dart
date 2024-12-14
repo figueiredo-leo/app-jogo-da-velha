87ddad2087c14e07c2fbc0daedfec56c53aacfd5
Leonardo da Silva Figueiredo <162579688+figueiredo-leo@users.noreply.github.com>
2024-12-14 16:05:49 -0300

    Update jogo_da_velha.dart

--- lib/componentes/jogo_da_velha.dart
+++ lib/componentes/jogo_da_velha.dart
@@ -1,186 +1,107 @@
-import 'dart:math';
-
+import 'package:expressions/expressions.dart';
 import 'package:flutter/material.dart';
 
-class JogoDaVelha extends StatefulWidget {
-  const JogoDaVelha({super.key});
-
+class Calculadora extends StatefulWidget {
+  const Calculadora({super.key});
   @override
-  State<JogoDaVelha> createState() => _JogoDaVelhaState();
+  State<Calculadora> createState() => _CalculadoraState();
 }
 
-class _JogoDaVelhaState extends State<JogoDaVelha> {
-  List<String> _tabuleiro = List.filled(9, '');
-  String _jogador = 'X';
-  bool _contraMaquina = false;
-  final Random _randomico = Random();
-  bool _pensando = false;
+class _CalculadoraState extends State<Calculadora> {
+  final String _limpar = 'Limpar';
+  String _expressao = '';
+  String _resultado = '';
 
-  void _iniciarJogo() {
+  void pressionarBotao(String valor) {
     setState(() {
-      _tabuleiro = List.filled(9, '');
-      _jogador = 'X';
-    });
-  }
-
-  void _trocaJogador() {
-    setState(() {
-      _jogador = _jogador == 'X' ? 'O' : 'X';
-    });
-  }
-
-  void _mostreDialogoVencedor(String vencedor) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(vencedor == 'Empate' ? 'Empate!' : 'Vencedor: $vencedor'),
-          actions: [
-            ElevatedButton(
-              child: const Text('Reiniciar Jogo'),
-              onPressed: () {
-                Navigator.of(context).pop();
-                _iniciarJogo();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  bool _verificaVencedor(String jogador) {
-    const posicoesVencedoras = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (var posicoes in posicoesVencedoras) {
-      if (_tabuleiro[posicoes[0]] == jogador &&
-          _tabuleiro[posicoes[1]] == jogador &&
-          _tabuleiro[posicoes[2]] == jogador) {
-        _mostreDialogoVencedor(jogador);
-        return true;
+      if (valor == _limpar) {
+        _expressao = '';
+        _resultado = ''; // Limpa o resultado ao pressionar "Limpar"
+      } else if (valor == '=') {
+        _calcularResultado();
+      } else {
+        _expressao += valor;
       }
-    }
-
-    if (!_tabuleiro.contains('')) {
-      _mostreDialogoVencedor('Empate');
-      return true;
-    }
-
-    return false;
-  }
-
-  void _jogadaComputador() {
-    setState(() =>_pensando = true);
-    
-    Future.delayed(const Duration(seconds: 1), () {
-      int movimento;
-      do {
-        movimento = _randomico.nextInt(9);
-      } while (_tabuleiro[movimento] != ' ');
-      setState(() {
-        _tabuleiro[movimento] = 'O';
-        if (!_verificaVencedor(_jogador)) {
-          _trocaJogador();
-        }
-        _pensando = false;
-      });
     });
   }
 
-  void _jogada(int index) {
-    if (_tabuleiro[index] == '') {
+  void _calcularResultado() {
+    try {
+      // Substitui os operadores para os padrões aceitos pelo ExpressionEvaluator
+      String expressaoCorrigida = _expressao.replaceAll('x', '*').replaceAll('÷', '/');
+
+      // Avalia a expressão
+      Expression expression = Expression.parse(expressaoCorrigida);
+      const evaluator = ExpressionEvaluator();
+      var resultado = evaluator.eval(expression, {});
+
+      // Atualiza o resultado com o valor calculado
       setState(() {
-        _tabuleiro[index] = _jogador;
-        if (!_verificaVencedor(_jogador)) {
-          _trocaJogador();
-          if (_contraMaquina && _jogador == 'O') {
-            _jogadaComputador();
-          }
-        }
+        _resultado = resultado.toString();
+      });
+    } catch (e) {
+      // Se ocorrer algum erro, exibe uma mensagem
+      setState(() {
+        _resultado = 'Erro, não foi possível calcular';
       });
     }
+  }
+
+  Widget _botao(String valor) {
+    return TextButton(
+      child: Text(
+        valor,
+        style: const TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onPressed: () => pressionarBotao(valor),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    double altura = MediaQuery.of(context).size.height * 0.5;
     return Column(
       children: [
         Expanded(
-          child: Row(
+          child: Text(
+            _expressao,
+            style: const TextStyle(fontSize: 24),
+          ),
+        ),
+        Expanded(
+          child: Text(
+            _resultado,
+            style: const TextStyle(fontSize: 24),
+          ),
+        ),
+        Expanded(
+          flex: 5,
+          child: GridView.count(
+            crossAxisCount: 5,
+            childAspectRatio: 2,
             children: [
-              Transform.scale(
-                scale: 0.6,
-                child: Switch(
-                  value: _contraMaquina,
-                  onChanged: (value) {
-                    setState(() {
-                      _contraMaquina = value;
-                      _iniciarJogo();
-                    });
-                  },
-                ),
-              ),
-              Text(_contraMaquina ? 'Computador' : 'Humano'),
-              const SizedBox(
-                width: 30.0,
-              ),
-              if (_pensando)
-                const SizedBox(
-                  height: 15.0,
-                  width: 15.0,
-                  child: CircularProgressIndicator(),
-                ),
+              _botao('7'),
+              _botao('8'),
+              _botao('9'),
+              _botao('x'),
+              _botao(','),
+              _botao('4'),
+              _botao('5'),
+              _botao('6'),
+              _botao('-'),
+              _botao('+'),
+              _botao('1'),
+              _botao('2'),
+              _botao('3'),
+              _botao('÷'),
+              _botao('='),
+              _botao('0'),
             ],
           ),
         ),
-        Expanded(
-          flex: 8,
-          child: SizedBox(
-            width: altura,
-            height: altura,
-            child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 5.0,
-                mainAxisSpacing: 17.0,
-              ),
-              itemCount: 9,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () => _jogada(index),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.blue[100],
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    child: Center(
-                      child: Text(
-                        _tabuleiro[index],
-                        style: const TextStyle(fontSize: 40.0),
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ),
-        Expanded(
-          child: ElevatedButton(
-            onPressed: _iniciarJogo,
-            child: const Text('Reiniciar Jogo'),
-          ),
-        ),
+        Expanded(child: _botao(_limpar))
       ],
     );
   }
